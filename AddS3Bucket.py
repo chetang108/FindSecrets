4adc99f50556c219fecd2c5d3358f0c91450c1b1
chetan181183 <chetan.10845688@ltimindtree.com>
2025-12-04 10:23:51 +0530

    after pull

--- AddS3Bucket.py
+++ AddS3Bucket.py
@@ -10,7 +10,7 @@ def create_s3_bucket(bucket_name, region=None):
             s3_client = boto3.client('s3', region_name=region)
             location = {'LocationConstraint': region}
             s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
-        print(f"✅ Bucket '{bucket_name}' created successfully on Fourth december.")
+        print(f"✅ Bucket '{bucket_name}' created successfully on Fourth december after pull.")
     except ClientError as e:
         print(f"❌ Error: {e}")
 
